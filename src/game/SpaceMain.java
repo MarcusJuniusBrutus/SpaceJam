601e8900e1f6fb75f998d617af79b4f714d68f3e
Kunala Deotare <kun.karan@yahoo.ca>
2022-12-13 19:43:25 -0500

    KeyListener + Timer

--- src/game/SpaceMain.java
+++ src/game/SpaceMain.java
@@ -4,40 +4,61 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 /**
  * Kunala, 2022 Dec 12
  */
 
-public class SpaceMain {
+public class SpaceMain implements ActionListener {
 	static final int PANW = 1200;
 	static final int PANH = 900;
-	Rectangle ship = new Rectangle(PANW / 2 - 50, PANH / 2 - 25, 100, 50);
+
+	DrawingPanel panel = new DrawingPanel();
+	SpaceShip ship = new SpaceShip();
+	
+	Timer t = new Timer(100, this);
 
 	public static void main(String[] args) {
-		new SpaceMain();
+		// using this makes animation more reliable
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new SpaceMain();
+			}
+		});
 	}
 
 	// holds the spaceship coordinates and sizes
-	private class Rectangle {
-		int x, y, l, h;
+	private class SpaceShip extends Rectangle {
+		double xx, yy;
+		double vx = 5, vy = 5;
+		Color clr = new Color (100, 0, 100);
+		static int width = 100;
+		static int height = 50;
 
-		Rectangle(int x, int y, int l, int h) {
-			this.x = x;
-			this.y = y;
-			this.l = l;
-			this.h = h;
+		SpaceShip() {
+			xx = PANW / 5;
+			yy = PANH / 2;
+
+			x = (int) xx;
+			y = (int) yy;
 		}
 	}
 
 	SpaceMain() {
 		JFrame window = new JFrame("SpaceJam");
-		DrawingPanel panel = new DrawingPanel();
+		
+		t.start();
 
 		window.add(panel);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -46,10 +67,12 @@ public class SpaceMain {
 		window.setVisible(true);
 	}
 
-	private class DrawingPanel extends JPanel {
+	private class DrawingPanel extends JPanel implements KeyListener {
 		DrawingPanel() {
 			this.setPreferredSize(new Dimension(PANW, PANH));
 			this.setBackground(new Color(168, 185, 190));
+			
+			this.addKeyListener(this);
 		}
 
 		@Override
@@ -65,7 +88,31 @@ public class SpaceMain {
 		}
 
 		void drawSpaceShip(Graphics g) {
-			g.drawRect(ship.x, ship.y, ship.l, ship.h);
+			g.setColor(ship.clr);
+			g.fillRect(ship.x, ship.y, ship.width, ship.height);
 		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				ship.yy -= 100;
+			}
+			panel.repaint();
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ship.xx += ship.vx;
+		ship.yy += ship.vy;
+		panel.repaint();
 	}
 }
